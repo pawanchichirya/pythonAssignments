1035651598a4e0aed942281f39a74f799d17cbf9
pawanchichirya <70518506+pawanchichirya@users.noreply.github.com>
2022-08-19 10:21:16 +0530

    Add files via upload

---
+++ assignment-1/file.py
@@ -0,0 +1,37 @@
+#1
+import sys
+print(sys.version)
+
+#2
+#typed "hello python" in shell
+
+#3
+#typed "python file.py" in cmd to run file
+
+#4 just ran the file in vs code
+
+#5
+print("Hello","Python",sep='\n')
+
+#6
+print('"MySirG"')
+
+#7
+print('"Teacher\'s Day"')
+
+#8
+name="pawan chichriya"
+print(name)
+
+#9
+name=input("please enter your name: ")
+age=int(input("please enter your age: "))
+qualification=input("please enter your qualification: ")
+yearsOfExp=int(input("please enter the years of exprience you have: "))
+print(name,age,qualification,yearsOfExp, sep="-")
+
+#10
+a=10
+b=12
+c=16
+print(a,b,c,sep="\n")
