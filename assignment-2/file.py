9952b55f242bd8606c2df880fd5706aab24f0a81
pawanchichirya <70518506+pawanchichirya@users.noreply.github.com>
2022-08-22 16:59:39 +0530

    Add files via upload

---
+++ assignment-2/file.py
@@ -0,0 +1,62 @@
+#1
+#in this program we are going to print "learning python" on scrren
+print('"Learning Python"')
+
+#2
+"""
+in this program we are going to print value of 4 variabls each in new line
+variable contains any value
+"""
+a="pc"
+b=10
+c=5.4
+d=True
+print(a,b,c,d,sep="\n")
+
+
+#3
+num=35
+boolean=True
+name="MySirG"
+floatingValue=5.46
+complexNum=3+4j
+print(type(num))
+print(type(boolean))
+print(type(name))
+print(type(floatingValue))
+print(type(complexNum))
+
+
+#4
+num1=10
+num2=10
+print(id(num1))
+print(id(num2))
+
+
+#5
+num=14
+name="pawan"
+floatingValue=5.48
+complexNum=5+8j
+print(num,type(num),id(num), sep='----')
+print(name,type(name),id(name), sep='----')
+print(floatingValue,type(floatingValue),id(floatingValue), sep='----')
+print(complexNum,type(complexNum),id(complexNum), sep='----')
+
+
+#6
+import keyword
+print(keyword.kwlist)
+
+
+#7(it's on shell, i can't attatch ss so writing the steps below
+#step-1 typed help() in shell
+#step-2 typed keywords
+
+#8 created two files named A0.py and A1.py
+
+#9 True,False,None
+
+#10 created new file named dateTime.py
+
